71b75789db21b8f49914f5d6a18f42324f21357f
Yasirud Dahanayake <yasiru@live.co.uk>
2017-05-10 17:01:24 +0100

    Added Swing Gui Support, Simple exception handling

--- Serverp/src/Server.java
+++ Serverp/src/Server.java
@@ -1,64 +1,88 @@
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-
-
-
-
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JTextPane;
+import javax.swing.JTextArea;
 
 public class Server
 {
-
 	private static ArrayList<ServerThread> clients;
 	private static ServerSocket sS;
 	private static final int PORT = 1234;
-	
-	
-	
-	
+	private static JFrame frame;
+	private static boolean serverRunning = true;
+	private static JTextArea textArea;
+
+	/**
+	 * Launch the application.
+	 */
 	public static void main(String[] args)
 	{
+		EventQueue.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				try
+				{
+					Server window = new Server();
+					window.frame.setVisible(true);
+				} catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+
 		SetUpConnections();
-	
+
 	}
-	
-	
+
+	/**
+	 * Create the application.
+	 */
+	public Server()
+	{
+		initialize();
+	}
+
 	/*
-	 * Listens to any connections,
-	 * if a Connection is made then adds it to to the list of connections 
+	 * Server starts up and listens for connections, if the server is running
+	 * then auto close
 	 */
 	private static void SetUpConnections()
 	{
 		try
 		{
-			sS= new ServerSocket(PORT);
-			while (true)
+			sS = new ServerSocket(PORT);
+			while (serverRunning)
 			{
-				
-				System.out.println("Debug : server is running");
+				System.out.println("Debug: server is running, waiting for connections");
 				Socket socket = sS.accept();
 				ServerThread rc = new ServerThread(socket);
-				clients.add(rc);
+				// clients.add(rc);
 				Thread tr = new Thread(rc);
 				tr.start();
-				
-				
-				
-				
-				;
-
 			}
 
+		} catch (BindException e)
+		{
+			JOptionPane.showMessageDialog(frame, "instance of a server is already running");
+			System.exit(0);
 		} catch (Exception e)
 		{
+
 			e.printStackTrace();
+
 		}
 	}
-	
-	
-	
+
 	/*
-	 * creates a new th
+	 * Runaable class to handle instances of clients that are connected
 	 */
 	private static class ServerThread implements Runnable
 	{
@@ -69,15 +93,26 @@ public class Server
 			this.socket = socket;
 		}
 
-		
 		@Override
 		public void run()
 		{
-			
-			
+
 		}
 
 	}
 
-	
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize()
+	{
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+
+		textArea = new JTextArea();
+		textArea.setBounds(46, 27, 370, 207);
+		frame.getContentPane().add(textArea);
+	}
 }
