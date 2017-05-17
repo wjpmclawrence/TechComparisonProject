c7ff5c603d2f3ac262e77621fdda3acc1686de50
Nathan Steer <nsteer93@gmail.com>
2017-05-17 19:41:34 +0100

    GENERAL - Added author signatures to all classes I have created
    REQUEST MANAGER
    - Renamed makeRequest method to requestMade, as I feel it more intuitively explains what the method is for
    - Updated RequestManager class to handle the creation of lists in seperate methods to de-clutter the requestMade method
    - Implemented sorting on the List that will be returned to the client when a sub-menu is requested. It should sort Languages in the list by similarity (I have not yet tested this, it may not work)
    
    VERSION MANAGER
    - Overhauled the VersionManager class to facilitate a persistant version number being stored on the server, even if the server is restarted
        + VersionManager no longer stores the version number as a variable
        + Version number is now stored in a .dat file
        + If no Version.dat file is found when either the getVersion method, or the updateVersion method is called, a file will be created with the version number 1.
    - Carried out some testing on the changes to the version manager. It appears to work, I will hand this class over to the testers for more rigorous testing.
    - Added extensive comments to both methods in this class

--- DataManager/src/DataManagement/RequestManager.java
+++ DataManager/src/DataManagement/RequestManager.java
@@ -1,13 +1,34 @@
 package DataManagement;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
+import Utils.Language;
+
+
+/**
+ * 
+ * @author Nathan Steer
+ *
+ */
 public class RequestManager
 {
-	private static boolean checkVersion ( int clientVer )
+	private static List<Object> checkVersion ( int clientVer )
 	{
-		return clientVer == VersionManager.getVersion();
+		List<Object> list = new ArrayList();
+		
+		if ( clientVer == VersionManager.getVersion() )
+		{
+			list.add( "version_ok" );
+		}
+		else
+		{
+			throw new UnsupportedOperationException( "Not Yet Implemented" );
+		}
+		
+		return list;
 	}
 	
 	private static List<Object> getSubMenu ( String langName )
@@ -33,7 +54,8 @@ public class RequestManager
 	 * @return			A list containing both the tag and any necessary data for the client
 	 * 
 	 */
-	public static List<Object> makeRequest ( String request )
+	@SuppressWarnings ( "unchecked" )
+	public static List<Object> requestMade ( String request )
 	{
 		String[] tmp = request.split( "|" );
 		List<Object> returnList = new ArrayList();
@@ -43,18 +65,14 @@ public class RequestManager
 			switch ( tmp[0] )
 			{
 				case "version":
-					if ( checkVersion( Integer.parseInt( tmp[1] ) ) )
-					{
-						returnList.add( "version_ok" );
-					}
-					else
-					{
-						throw new UnsupportedOperationException( "Not Fully Implemented" );
-						// returnList.add( "menu_list" );
-					}
+					returnList = checkVersion( Integer.parseInt( tmp[1] ) );
 					break;
+				
 				case "request":
 					returnList = getSubMenu( tmp[1] );
+					Collections.sort( (List<Language>) (Object) returnList, (Comparator<Language>) ( l1, l2 ) -> Integer
+							.compare( l1.getSimilarity(), l2.getSimilarity() ) );
+					returnList.add( 0, "sub_menu_list" );
 					break;
 			}
 		}
--- DataManager/src/Utils/package-info.java
+++ DataManager/src/Utils/package-info.java
@@ -1,7 +1,4 @@
 /**
- * 
- */
-/**
  * @author Nathan Steer
  *
  */
