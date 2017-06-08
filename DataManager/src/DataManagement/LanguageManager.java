818cc694fc8cea692b29892e066522ee41ed33b6
Nathan Steer <nsteer93@gmail.com>
2017-06-08 12:50:09 +0100

    Added class skeleton for LanguageManager

---
+++ DataManager/src/DataManagement/LanguageManager.java
@@ -0,0 +1,20 @@
+package DataManagement;
+
+import java.util.List;
+
+import Utils.Language;
+
+public class LanguageManager
+{
+	static List<Language> langList;
+	
+	public static List<Language> setupLanguages ( String[][] incoming )
+	{
+		return langList;
+	}
+	
+	private static String calcWeeks ( int similarity )
+	{
+        throw new UnsupportedOperationException( "Not Yet Implemented" );
+	}
+}
