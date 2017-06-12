c80773d374cea57684fcc27867e94766c91cb66a
Nathan Steer <nsteer93@gmail.com>
2017-06-12 16:37:00 +0100

    - Adjusted the size of the GUI

--- Serverp/src/ClientInterface/ServerGUI.java
+++ Serverp/src/ClientInterface/ServerGUI.java
@@ -1,51 +1,55 @@
 package ClientInterface;
+
+import java.awt.FlowLayout;
+
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 public class ServerGUI
 {
-	private static JFrame sInterface;
-	private static JScrollPane scrollPane;
-	private static JTextArea textArea;
-
-	public static JScrollPane getScrollPane()
+	private static JFrame		sInterface;
+	private static JScrollPane	scrollPane;
+	private static JTextArea	textArea;
+	
+	public static JScrollPane getScrollPane ()
 	{
 		return scrollPane;
 	}
-
-	public static JTextArea getTextArea()
+	
+	public static JTextArea getTextArea ()
 	{
 		return textArea;
 	}
-
-	public static JFrame getsInterface()
+	
+	public static JFrame getsInterface ()
 	{
 		return sInterface;
 	}
-
+	
 	public ServerGUI()
 	{
-
+		
 		initialize();
-		sInterface.setVisible(true);
+		sInterface.setVisible( true );
 	}
-
-	private void initialize()
+	
+	private void initialize ()
 	{
 		sInterface = new JFrame();
-		sInterface.setBounds(100, 100, 450, 300);
-		sInterface.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		sInterface.getContentPane().setLayout(null);
-		sInterface.setTitle("Server");
-		sInterface.setResizable(false);
+		sInterface.setBounds( 100, 100, 450, 300 );
+		sInterface.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		sInterface.setLayout( null );
+		sInterface.setTitle( "Server" );
+		sInterface.setExtendedState( JFrame.MAXIMIZED_BOTH );
+		sInterface.setUndecorated( true );
+		sInterface.setVisible( true );
 		scrollPane = new JScrollPane();
-		scrollPane.setBounds(27, 21, 400, 235);
-		sInterface.getContentPane().add(scrollPane);
+		scrollPane.setBounds( 0, 0, sInterface.getWidth(), sInterface.getHeight() );
+		sInterface.getContentPane().add( scrollPane );
 		textArea = new JTextArea();
-		textArea.setEditable(false);
-		scrollPane.setViewportView(textArea);
+		textArea.setEditable( false );
+		scrollPane.setViewportView( textArea );
 		
-
 	}
 }
