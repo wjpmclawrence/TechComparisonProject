c2bdce88038e1be8b3ae59d43c642944c141b465
Ben <ben@chelub.com>
2017-06-12 17:09:36 +0100

    Created List_layout, Started work on FormattedArrayAdaptor.
    - Created a prototype for list_layout
    - Started work on the getView method of FormattedArrayAdaptor
    - Got rid of the unused LanguageData class

--- Client/app/src/main/java/com/example/benbody/client/FormattedArrayAdaptor.java
+++ Client/app/src/main/java/com/example/benbody/client/FormattedArrayAdaptor.java
@@ -1,9 +1,12 @@
 package com.example.benbody.client;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -31,7 +34,16 @@ public class FormattedArrayAdaptor<Language> extends ArrayAdapter<Language>
     @Override // called to determine how each element in the list will be displayed
     public View getView(int position, View convertView, ViewGroup parent)
     {
-        // TODO develop XML file to define individual list elements
+        // inflater is used to inflate the layout XML file
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.list_layout, parent, false);
+
+        // assigns each of the elements of the layout to a variable
+        TextView langName = (TextView) rowView.findViewById(R.id.langName);
+        TextView noWeeks = (TextView) rowView.findViewById(R.id.noWeeks);
+        TextView percentage = (TextView) rowView.findViewById(R.id.percentage);
+        ProgressBar percProgress = (ProgressBar) rowView.findViewById(R.id.percentageProgress);
         // TODO display Language objects in the list format
         return null;
     }
