8a82989925fa142e9febc4ae08b6e85a6949c0e8
W J P Mclawrence <william_mclawrence@hotmail.co.uk>
2017-06-14 15:22:44 +0100

    Updated Utils Changed IP in TCP client

--- Client/app/src/main/java/Utils/Language.java
+++ Client/app/src/main/java/Utils/Language.java
@@ -1,60 +1,86 @@
 package Utils;
 
+import java.io.Serializable;
+
 /**
- * 
- * @author Nathan Steer
- * 
- * Class that holds data about the languages: name, training time, similarity.
+ * Holds data about the languages: name, training time, similarity.
  * <p>
  * Data Stored:
- * 		*	name:			The name of the language
- * 		*	trainingTime:	The amount of time it would take to train an individual on this language
- * 		*	similarity:		How similar this language is to the specified language
+ * <p>
+ * name: The name of the language
+ * <p>
+ * trainingTime: The amount of time it would take to train an individual on this language
+ * <p>
+ * similarity: How similar this language is to the specified language
+ * 
+ * @author Nathan Steer
  *
  */
-public class Language
+public class Language implements Comparable<Language>, Serializable
 {
-	private String	name;			// Name of this language
-	private String	trainingTime;	// Amount of time it would take to be trained based on similarity
-	private int		similarity;		// Similarity to the language this is being compared to
+	private static final long	serialVersionUID	= 1L;
+	private String				name;			// Name of this language
+	private String				trainingTime;	// Amount of time it would take to be trained based on similarity
+	private int					similarity;		// Similarity to the language this is being compared to
 	
-	Language( String name, int similarity, String trainingTime )
+	public Language( String name, int similarity, String trainingTime )
 	{
-		this.name			= name;
-		this.similarity		= similarity;
-		this.trainingTime	= trainingTime;
+		this.name = name;
+		this.similarity = similarity;
+		this.trainingTime = trainingTime;
 	}
 	
+	public Language( String name, int similarity )
+	{
+		this.name = name;
+		this.similarity = similarity;
+	}
 	
 	/**
 	 * Used to obtain the stored name
 	 * 
-	 * @return	A String containing the name of the language
+	 * @return A String containing the name of the language
 	 */
 	public String getName ()
 	{
 		return name;
 	}
 	
-	
 	/**
 	 * Used to obtain the stored trainingTime
 	 * 
-	 * @return	A String containing the estimated time it would take to train someone
+	 * @return A String containing the estimated time it would take to train someone
 	 */
 	public String getTrainingTime ()
 	{
 		return trainingTime;
 	}
 	
-	
 	/**
 	 * Used to obtain the stored similarity
 	 * 
-	 * @return	An int containing the percentage similarity of this language and the specified one
+	 * @return An int containing the percentage similarity of this language and the specified one
 	 */
 	public int getSimilarity ()
 	{
 		return similarity;
+	}
+	
+	/**
+	 * Comparator method, allowing Languages to be compared to one another by similarity
+	 */
+	@Override
+	public int compareTo ( Language other )
+	{
+		return Integer.compare( other.getSimilarity(), similarity );
+	}
+	
+	/**
+	 * String override method, allowing Language objects to be output as a meaningful string
+	 */
+	@Override
+	public String toString ()
+	{
+		return name + "	:	" + similarity + "%" + "	:	" + trainingTime;
 	}
 }
